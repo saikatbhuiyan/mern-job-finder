62cd3a0c2ea4bac449cb192b414edfa65c9502b9
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-07-26 03:02:00 +0600

    Client Error Page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
