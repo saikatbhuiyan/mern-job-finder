774827fdab5c8aede46387e98f44af62593d025d
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-07-26 00:33:34 +0600

    Logo Component

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,12 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         {/* info */}
