02ed306281c139c6073be9c4a50edcacbb4d1955
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-08-06 17:06:09 +0600

    Server Set

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
