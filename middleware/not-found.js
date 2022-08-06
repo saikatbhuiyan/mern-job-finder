3939d6f3824d2cb1b36135b4e38336d11a4893a9
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-08-06 17:35:49 +0600

    NotFoundMiddleware

---
+++ middleware/not-found.js
@@ -0,0 +1,5 @@
+const notFoundMiddleware = (req, res) => {
+  res.status(404).send("Route does not exist");
+};
+
+export default notFoundMiddleware;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
+
+// middleware
+import notFoundMiddleware from "./middleware/not-found.js";
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
