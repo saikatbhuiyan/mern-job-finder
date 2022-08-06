499ecb93b1feba937121c94d2769c5d814bd3579
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-08-07 03:15:29 +0600

    User Model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    select: false,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "my city",
+  },
+});
+
+export default mongoose.model("User", UserSchema);
