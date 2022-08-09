9c319d8dcf852cdfae7b96a7fda9d158ed55f116
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-08-10 05:48:28 +0600

    Hashing password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -36,6 +37,11 @@ const UserSchema = new mongoose.Schema({
     maxlength: 20,
     default: "my city",
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model("User", UserSchema);
