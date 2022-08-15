2cf0c5681c967819ee259f2c66917a9acd30891d
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-08-16 02:42:42 +0600

    Big Sidebar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,7 +1,26 @@
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
 import Wrapper from "../assets/wrappers/BigSidebar";
 
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { showSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container " : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
