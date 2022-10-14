361f947a30e49d43dcf86073e1553441e449cd4a
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-10-14 15:29:42 +0600

    Server Update  Modified paths

--- models/User.js
+++ models/User.js
@@ -41,8 +41,11 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("save", async function () {
-  // const salt = await bcrypt.genSalt(10);
-  // this.password = await bcrypt.hash(this.password, salt);
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified("name"));
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.createJWT = function () {
